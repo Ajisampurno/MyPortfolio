a0c8fb307c422bca0e592ff8a37ce39a96be2ff9
aji sampurno <ajisampurno121@gmail.com>
2024-07-18 16:25:10 +0700

    create, layout halaman Home

---
+++ src/components/Ulasan.vue
@@ -0,0 +1,85 @@
+<template>
+  <h2 class="text-center">Ulasan</h2>
+  <!-- Three columns of text below the carousel -->
+  <div class="row mt-5">
+    <div class="colom-ulasan col-lg-4">
+      <svg
+        class="bd-placeholder-img rounded-circle m-auto"
+        width="140"
+        height="140"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-label="Placeholder: 140x140"
+        preserveAspectRatio="xMidYMid slice"
+        focusable="false"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#777" />
+        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+      </svg>
+
+      <h2 class="text-center">Heading</h2>
+      <p class="text-center">
+        Some representative placeholder content for the three columns of text below the carousel.
+        This is the first column.
+      </p>
+      <p class="text-center"><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.col-lg-4 -->
+    <div class="col-lg-4">
+      <svg
+        class="bd-placeholder-img rounded-circle"
+        width="140"
+        height="140"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-label="Placeholder: 140x140"
+        preserveAspectRatio="xMidYMid slice"
+        focusable="false"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#777" />
+        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+      </svg>
+
+      <h2 class="text-center">Heading</h2>
+      <p class="text-center">
+        Another exciting bit of representative placeholder content. This time, we've moved on to the
+        second column.
+      </p>
+      <p class="text-center"><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.col-lg-4 -->
+    <div class="col-lg-4">
+      <svg
+        class="bd-placeholder-img rounded-circle"
+        width="140"
+        height="140"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-label="Placeholder: 140x140"
+        preserveAspectRatio="xMidYMid slice"
+        focusable="false"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#777" />
+        <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+      </svg>
+
+      <h2 class="text-center">Heading</h2>
+      <p class="text-center">
+        And lastly this, the third column of representative placeholder content.
+      </p>
+      <p class="text-center"><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
+    </div>
+    <!-- /.col-lg-4 -->
+  </div>
+  <!-- /.row -->
+</template>
+
+<style>
+.bd-placeholder-img {
+  display: block;
+  margin: 0 auto;
+}
+</style>
